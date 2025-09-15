81270f3d1a05a7813e36a1e0b857f2fb4b33d922
Bhavika9394 <161156806+Bhavika9394@users.noreply.github.com>
2025-09-15 18:51:08 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+let age=18 //number
+let name="bhaivka" //string
+
+//number=> 2 to power 53
+//bigInt
+
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined =>when you not assign value
+//symbol =>unique
+
+
+//object
+
+console.log(typeof "bhavika");
