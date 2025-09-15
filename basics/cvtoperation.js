3cb158889e92528ff00dc038c417c66cf52ae109
Bhavika9394 <161156806+Bhavika9394@users.noreply.github.com>
2025-09-15 19:07:31 +0000

    convert operation

---
+++ basics/cvtoperation.js
@@ -0,0 +1,20 @@
+let score="33";
+console.log(typeof score);
+console.log(typeof(score))
+
+//convert into number
+let valueIntNumber=Number(score)
+console.log(typeof valueIntNumber);
+
+//"33"=33
+//"33abc"=>NaN
+//true=1, false=0
+
+//1 =>true
+//0=false
+// ""=false
+//"bhavika"=true
+
+let someNumber=33
+let stringNum=String(someNumber)
+console.log(typeof stringNum);
