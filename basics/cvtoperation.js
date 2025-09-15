f524263211a114229bc0f53042fb7e2becd4a26a
Bhavika9394 <161156806+Bhavika9394@users.noreply.github.com>
2025-09-15 19:22:15 +0000

    convertion opreration

--- basics/cvtoperation.js
+++ basics/cvtoperation.js
@@ -18,3 +18,23 @@ console.log(typeof valueIntNumber);
 let someNumber=33
 let stringNum=String(someNumber)
 console.log(typeof stringNum);
+
+
+//*****operation */
+let value=3
+let negValue=-value
+console.log(negValue);
+
+// basics opertation +,-,*,**(power),%(remider),/(quotient)
+let str1="bhavika"
+let str2=" gupta"
+let str3=str1+str2;
+
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);//122
+console.log(2+ 1+"2");
+
+
