7c938f0c15ca0cf0187cbd5ebe47c930887164df
Bhavika9394 <161156806+Bhavika9394@users.noreply.github.com>
2025-09-15 18:49:06 +0000

    variable

---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const accountId=12345
+let accountEmail="bhavika@gmail.com"
+var acconutPassword="12345"
+accountCity="jaipur"
+
+// accountId=2 not allowed
+accountEmail="fdf@.com"
+acconutPassword="1234"
+accountCity="jaipur"
+console.table([accountId,accountEmail,acconutPassword,accountCity])
+/*
+dont use var 
+because of issue in block scope and function scope
+*/
