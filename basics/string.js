8446e0df130138acdfd25025ea2dbbf0cf9dde25
Bhavika9394 <161156806+Bhavika9394@users.noreply.github.com>
2025-09-16 14:19:11 +0000

    string

---
+++ basics/string.js
@@ -0,0 +1,16 @@
+// concat string
+const name ="bhavika"
+const repo=5
+console.log(`hello my name is ${name} and repo ${repo}`);
+// declare string
+const gamename=new String('bhavika')
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+const string1=new String("bhavika")
+console.log(string1);
+console.log(string1.replace("bhavika","pranshu"));
+console.log(string1.includes('h'));
+
